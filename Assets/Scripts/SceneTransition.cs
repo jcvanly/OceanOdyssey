cd9d3d960c3c8f1de334885a8f070bfc0958bb6a
Jack Connor Vanlyssel <jackvanlyssel@gmail.com>
2024-01-28 21:36:54 -0700

    Adding in scene changes

---
+++ Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !other.isTrigger)
+        {
+            LoadRandomScene();
+        }
+
+        Debug.Log("Trigger entered");
+    }
+
+    private void LoadRandomScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int sceneCount = SceneManager.sceneCountInBuildSettings;
+        int randomSceneIndex;
+
+        do
+        {
+            // Assuming you want to skip the first scene (index 0) and start from index 1
+            randomSceneIndex = Random.Range(2, sceneCount);
+        } 
+        while (randomSceneIndex == currentSceneIndex);
+
+        Debug.Log("Loading scene index: " + randomSceneIndex);
+        SceneManager.LoadScene(randomSceneIndex);
+    }
+}
