dbff84a3bf28c9c6a8ee7169090c41adb0d436d2
Ikirochi <danielprairie1@gmail.com>
2024-02-08 18:24:30 -0700

    powerup drops
    powerup drops when enemy is killed pt 1

--- Assets/Enemies/EnemyDeath.cs
+++ Assets/Enemies/EnemyDeath.cs
@@ -7,11 +7,28 @@ public class EnemyDeath : MonoBehaviour
 {
     public static event Action OnDeath;
 
+    // Reference to the power-up prefab
+    public GameObject powerUpPrefab;
+
     public void Die()
     {
-        //Debug.Log("I died");
+        // Always spawn the power-up for testing purposes
+        SpawnPowerUp();
+
+        // Invoke the OnDeath event
         OnDeath?.Invoke();
-        GlobalEnemyManager.EnemyDied(); // Decrement global counter
+
+        // Decrement global counter
+        GlobalEnemyManager.EnemyDied();
+
+        // Destroy the enemy gameObject
         Destroy(gameObject);
+    }
+
+    // Function to spawn the power-up
+    private void SpawnPowerUp()
+    {
+        // Instantiate the power-up prefab at the enemy's position
+        Instantiate(powerUpPrefab, transform.position, Quaternion.identity);
     }
 }
