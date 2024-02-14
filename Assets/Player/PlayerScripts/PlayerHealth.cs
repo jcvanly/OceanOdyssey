610b3a3fe8442582445a371dc3c395d3e536ca80
jenieto427 <jenieto@unm.edu>
2024-02-13 22:43:15 -0700

    Player Flashes when damaged

--- Assets/Player/PlayerScripts/PlayerHealth.cs
+++ Assets/Player/PlayerScripts/PlayerHealth.cs
@@ -10,16 +10,46 @@ public class PlayerHealth : MonoBehaviour
     public SpriteRenderer playerSr;
     public PlayerMovement playerMovement;
     // Start is called before the first frame update
+    public float flashDuration = 1f;
+    private Color damageColor = new Color(1f, 0f, 0f, 0.5f);
+    private Color originalColor;
+    private float damageTime;
+    private float timeSinceDamage;
+    private float timeLastFlash;
+    private float currTime;
+    private bool flashOnDamage = false;
+    
     void Start()
     {
         DontDestroyOnLoad(gameObject);
         health = maxHealth;
+        originalColor = playerSr.color;
        // DontDestroyOnLoad(gameObject);
     }
 
     private void Update()
     {
         PlayerPrefs.SetInt("PlayerHealth", health);
+        currTime = Time.time;
+
+        if(currTime >= (damageTime + flashDuration) && flashOnDamage == true)
+        {
+            resetColor();
+        }
+        else if (flashOnDamage == true)
+        {
+            if(playerSr.color == originalColor && currTime >= (timeLastFlash + .1f))
+            {
+                timeLastFlash = currTime;
+                playerSr.color = damageColor;
+            }
+            else if (playerSr.color == damageColor && currTime >= (timeLastFlash + .1f))
+            {
+                timeLastFlash = currTime;
+                playerSr.color = originalColor;
+            }
+        }
+
     }
 
 
@@ -33,5 +63,20 @@ public class PlayerHealth : MonoBehaviour
             playerSr.enabled = false;
             playerMovement.enabled = false;
         }
+        else
+        {
+            currTime = Time.time;
+            flashOnDamage = true;
+            damageTime = currTime;
+            timeLastFlash = currTime;
+            playerSr.color = damageColor;
+        }
+
+    }
+
+    public void resetColor()
+    {
+        playerSr.color = originalColor;
+        flashOnDamage = false;
     }
 }
