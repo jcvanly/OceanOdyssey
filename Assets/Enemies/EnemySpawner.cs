cb17c3459e0302c4c0957bb848d48e02bb98589c
jenieto427 <jenieto@unm.edu>
2024-02-28 14:00:39 -0700

    updated enemy spawn logic

--- Assets/Enemies/EnemySpawner.cs
+++ Assets/Enemies/EnemySpawner.cs
@@ -6,6 +6,7 @@ public class EnemySpawner : MonoBehaviour
 {
     public GameObject[] enemyPrefabs;
     private bool hasSpawned = false; // Flag to check if enemy has been spawned
+    
 
     void Start()
     {
@@ -16,7 +17,23 @@ public class EnemySpawner : MonoBehaviour
     {
         if (!hasSpawned)
         {
-            int randomIndex = Random.Range(0, enemyPrefabs.Length);
+            bool krakenDead = GlobalEnemyManager.KrakenDefeated;
+            bool crabLobsterDead  = GlobalEnemyManager.CrabAndLobsterDead();
+            int randomIndex;
+
+            if(krakenDead && crabLobsterDead == false)
+            {
+                randomIndex = Random.Range(0, enemyPrefabs.Length - 2);
+            }
+            else if (krakenDead && crabLobsterDead)
+            {
+                randomIndex = Random.Range(0, enemyPrefabs.Length - 2);
+            }
+            else
+            {
+                randomIndex = Random.Range(0,enemyPrefabs.Length - 3);
+            }
+            
             GameObject randomEnemyPrefab = enemyPrefabs[randomIndex];
             Instantiate(randomEnemyPrefab, transform.position, Quaternion.identity);
             GlobalEnemyManager.EnemySpawned(); // Increment global counter
