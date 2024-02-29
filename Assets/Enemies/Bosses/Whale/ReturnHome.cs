2b5b74628c1093d8ec6f539e9908deec5f9fb46a
Ikirochi <danielprairie1@gmail.com>
2024-02-29 16:53:46 -0700

    victory fixes
    victory fixes

--- Assets/Enemies/Bosses/Whale/ReturnHome.cs
+++ Assets/Enemies/Bosses/Whale/ReturnHome.cs
@@ -5,10 +5,37 @@ using UnityEngine.SceneManagement; // Required for scene management
 
 public class ReturnHomeButton : MonoBehaviour
 {
+    public Destroyer destroyer;
     // Method to load the first scene
     public void LoadFirstScene()
     {
+        if (GlobalEnemyManager.TotalEnemies != 0)
+        {
+            for (int i = 0; i <= GlobalEnemyManager.TotalEnemies; i++)
+            {
+                GlobalEnemyManager.EnemyDied();
+                GlobalEnemyManager.EnemyDied();
+            }
+        }
 
+        if (GlobalEnemyManager.ScenesVisited != 0)
+        {
+            for (int i = 0; i <= GlobalEnemyManager.ScenesVisited; i++)
+            {
+                GlobalEnemyManager.DecreaseScenesVisited();
+                GlobalEnemyManager.DecreaseScenesVisited();
+            }
+        }
+        if (GlobalEnemyManager.KrakenDefeated == true)
+        {
+            GlobalEnemyManager.KrakenNotDefeated();
+        }
+        if (GlobalEnemyManager.CrabDefeated == true)
+        {
+            GlobalEnemyManager.CrabNotDefeated();
+        }
+
+        destroyer.Destroy();
         SceneManager.LoadScene(0);
     }
 }
